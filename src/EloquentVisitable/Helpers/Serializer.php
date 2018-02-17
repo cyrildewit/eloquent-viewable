14771719be19f4462ff11741ee23a97d102ae670
Cyril de Wit <cyrilwit@gmail.com>
2018-02-17 17:48:47 +0100

    feat: add the visit counter serializer

---
+++ src/EloquentVisitable/Helpers/Serializer.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eloquent Visitable.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentVisitable\Helpers;
+
+/**
+ * This is the serializer helper.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+class Serializer
+{
+    /**
+     * Return the right type, based upon the given options.
+     *
+     * Available types:
+     * - normal
+     * - unique
+     *
+     * @return string
+     */
+    public function createType($unique): string
+    {
+        // If unique option was given, it's: 'unique' otherwise it's: 'normal'
+        return $unique ? 'unique' : 'normal';
+    }
+
+    /**
+     * Create a period from two dates.
+     *
+     * @return string
+     */
+    public function createPeriod($sinceDate, $uptoDate, $now)
+    {
+        $sinceDateString = $sinceDate ? $sinceDate->diffInSeconds($now) : '';
+        $uptoDateString = $uptoDate ? $uptoDate->diffInSeconds($now) : '';
+
+        return "{$sinceDateString}|{$uptoDateString}";
+    }
+}
