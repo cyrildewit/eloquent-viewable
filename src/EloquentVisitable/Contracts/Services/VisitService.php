6bbbbafa280285ffc26e4195aa6e5ff781d6e807
Cyril de Wit <cyrilwit@gmail.com>
2018-02-18 17:30:29 +0100

    refactor: require illuminate packages

---
+++ src/EloquentVisitable/Contracts/Services/VisitService.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eloquent Visitable.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentVisitable\Contracts\Services;
+
+/**
+ * Contract for the Visit Serviec.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+interface VisitService
+{
+    //
+}
