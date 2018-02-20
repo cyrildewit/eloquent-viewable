3f4eb025f0d79104855f019e52349d89e2ad427a
Cyril de Wit <cyrilwit@gmail.com>
2018-02-20 20:10:48 +0100

    refactor: add base TestCase

---
+++ tests/Tests/TestCase.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eloquent Visitable.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentVisitable\Tests;
+
+use Illuminate\Support\Facades\File;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+/**
+ * This is the abstract test case class.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+abstract class TestCase extends Orchestra
+{
+    protected $configFileName = 'eloquent-visitable';
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->destroyPackageMigrations();
+        $this->publishPackageMigrations();
+        $this->migratePackageTables();
+        $this->migrateUnitTestTables();
+        $this->registerPackageFactories();
+    }
+
+    /**
+     * Load package service provider.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            \CyrildeWit\EloquentVisitable\EloquentVisitableServiceProvider::class,
+            \Orchestra\Database\ConsoleServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Publish package migrations.
+     *
+     * @return void
+     */
+    public function publishPackageMigrations()
+    {
+        $this->artisan('vendor:publish', ['--force' => '', '--tag' => 'migrations']);
+    }
+
+    /**
+     * Delete all published migrations.
+     *
+     * @return void
+     */
+    public function destroyPackageMigrations()
+    {
+        File::cleanDirectory('vendor/orchestra/testbench-core/laravel/database/migrations');
+    }
+
+    /**
+     * Perform package database migrations.
+     *
+     * @return void
+     */
+    protected function migratePackageTables()
+    {
+        $this->loadMigrationsFrom([
+            // '--database' => 'sqlite',
+            '--realpath' => database_path('migrations'),
+        ]);
+    }
+
+    /**
+     * Perform unit test database migrations.
+     *
+     * @return void
+     */
+    protected function migrateUnitTestTables()
+    {
+        $this->loadMigrationsFrom([
+            // '--database' => 'sqlite',
+            '--realpath' => realpath(__DIR__.'/../../database/migrations'),
+        ]);
+    }
+
+    /**
+     * Register package related model factories.
+     *
+     * @return void
+     */
+    protected function registerPackageFactories()
+    {
+        $pathToFactories = realpath(__DIR__.'/../../database/factories');
+        $this->withFactories($pathToFactories);
+    }
+}
