843f54451ed8782bf85bb911bf260010ea1b2f1b
Cyril de Wit <cyrilwit@gmail.com>
2018-03-12 19:48:27 +0100

    feat: add PastType enum class

---
+++ src/Enums/PastType.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eloquent Viewable.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable\Enums;
+
+/**
+ * This is the visit service manager.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+abstract class PastType
+{
+    const PAST_SECONDS = 'PAST_SECONDS';
+    const PAST_MINUTES = 'PAST_MINUTES';
+    const PAST_DAYS = 'PAST_DAYS';
+    const PAST_WEEKS = 'PAST_WEEKS';
+    const PAST_MONTHS = 'PAST_MONTHS';
+    const PAST_YEARS = 'PAST_YEARS';
+}
