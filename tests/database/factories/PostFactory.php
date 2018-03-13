26f58967146f86e5673707259910fae17ee4daae
Cyril de Wit <cyrilwit@gmail.com>
2018-03-13 20:03:17 +0100

    chore(tests): add Post model with factory

---
+++ tests/database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eloquent Viewable.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Faker\Generator as Faker;
+use CyrildeWit\EloquentViewable\Tests\Stubs\Models\Post;
+
+/*
+ * This is the Post factory.
+ *
+ * @var \Illuminate\Database\Eloquent\Factory  $factory
+ */
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->title,
+        'body' => $faker->paragraph,
+    ];
+});
