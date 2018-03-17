dd20dfaf8028a572995ffbff0697b51b68f4e10c
Cyril de Wit <cyrilwit@gmail.com>
2018-03-17 19:06:33 +0100

    feat: add Viewable contract

---
+++ src/Contracts/Traits/Viewable.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eloquent Viewable.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable\Contracts\Traits;
+
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+/**
+ * Interface Viewable.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+interface Viewable
+{
+    //
+}
