6e7c266e2a45408bcb1ec7621a5620b89953b5aa
Cyril de Wit <cyrilwit@gmail.com>
2018-03-23 22:49:30 +0100

    feat: make cookie name configurable and change default name

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::middleware(['web'])->group(function () {
-    $cookieName = config('eloquent-viewable.cookie_name', 'ELOQUENT_VIEWABLE_COOKIE');
+    $cookieName = config('eloquent-viewable.cookie_name', 'eloquent_viewable');
 
     if (Cookie::get($cookieName) == false) {
         Cookie::queue($cookieName, str_random(80), 2628000);
