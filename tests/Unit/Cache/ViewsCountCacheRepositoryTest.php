d196563059f2e95b8d85748321a97535fc92c336
Cyril de Wit <cyrilwit@gmail.com>
2018-03-25 17:57:50 +0200

    test: add tests for the ViewsCountCacheRepository class

--- tests/Unit/Cache/ViewsCountCacheRepositoryTest.php
+++ tests/Unit/Cache/ViewsCountCacheRepositoryTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace CyrildeWit\EloquentViewable\Tests\Unit\Cache;
 
+use Cache;
 use CyrildeWit\EloquentViewable\Tests\TestCase;
 use CyrildeWit\EloquentViewable\Cache\ViewsCountCacheRepository;
 
@@ -29,5 +30,35 @@ class ViewsCountCacheRepositoryTest extends TestCase
         $cacheRepository = $this->app->make(ViewsCountCacheRepository::class);
 
         $this->assertInstanceOf(ViewsCountCacheRepository::class, $cacheRepository);
+    }
+
+    /** @test */
+    public function it_can_put_a_views_count_in_the_cache()
+    {
+        $cacheRepository = $this->app->make(ViewsCountCacheRepository::class);
+
+        $cacheRepository->put('something', 3500);
+
+        $this->assertEquals(3500, Cache::get(config('eloquent-viewable.cache.key').'.something'));
+    }
+
+    /** @test */
+    public function it_can_get_a_views_count_from_the_cache()
+    {
+        $cacheRepository = $this->app->make(ViewsCountCacheRepository::class);
+
+        Cache::set(config('eloquent-viewable.cache.key').'.something', 1500, 20);
+
+        $this->assertEquals(1500, $cacheRepository->get('something'));
+    }
+
+    /** @test */
+    public function it_can_check_a_views_count_in_the_cache()
+    {
+        $cacheRepository = $this->app->make(ViewsCountCacheRepository::class);
+
+        Cache::set(config('eloquent-viewable.cache.key').'.something', 2200, 20);
+
+        $this->assertEquals(true, $cacheRepository->has('something'));
     }
 }
