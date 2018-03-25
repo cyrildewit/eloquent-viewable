75ba8641d980c9d7ae1765bb82cdd92300bf0bc6
Cyril de Wit <cyrilwit@gmail.com>
2018-03-25 18:15:48 +0200

    test: add tests for ProcessView job

---
+++ tests/Unit/Jobs/ProcessViewTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eloquent Viewable.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable\Tests\Unit\Jobs;
+
+use Config;
+use Illuminate\Support\Facades\Queue;
+use CyrildeWit\EloquentViewable\Tests\TestCase;
+use CyrildeWit\EloquentViewable\Jobs\ProcessView;
+use CyrildeWit\EloquentViewable\Tests\Stubs\Models\Post;
+
+/**
+ * Class ProcessViewTest.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+class ProcessViewTest extends TestCase
+{
+    /** @test */
+    public function it_gets_pushed_to_the_queue_when_saving_views()
+    {
+        Queue::fake();
+
+        $post = factory(Post::class)->create();
+        Config::set('eloquent-viewable.jobs.store_new_view.enabled', true);
+
+        $post->addView();
+
+        Queue::assertPushed(ProcessView::class, function ($job) {
+            return $job->view->id === 1;
+        });
+    }
+
+    /** @test */
+    public function it_gets_pushed_to_the_queue_when_saving_a_view_and_saves_it_in_the_database()
+    {
+        Queue::fake();
+
+        $post = factory(Post::class)->create();
+        Config::set('eloquent-viewable.jobs.store_new_view.enabled', true);
+
+        $post->addView();
+
+        Queue::assertPushed(ProcessView::class, function ($job) {
+            return $job->view->id === 1;
+        });
+
+        $this->assertEquals(1, $post->getViews());
+    }
+}
