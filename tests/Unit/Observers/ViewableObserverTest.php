a24b57531f2a7048cce75f604b23c241c249c020
Cyril de Wit <cyrilwit@gmail.com>
2018-03-26 17:57:56 +0200

    test: remove all views when deleted

---
+++ tests/Unit/Observers/ViewableObserverTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eloquent Viewable.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable\Tests\Unit\Observers;
+
+use CyrildeWit\EloquentViewable\Models\View;
+use CyrildeWit\EloquentViewable\Tests\TestCase;
+use CyrildeWit\EloquentViewable\Tests\Stubs\Models\Post;
+
+/**
+ * Class ViewableObserverTest.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+class ViewableObserverTest extends TestCase
+{
+    /** @test */
+    public function it_removes_all_views_when_deleted()
+    {
+        $post = factory(Post::class)->create();
+
+        $post->addView();
+        $post->addView();
+        $post->addView();
+
+        $this->assertEquals(3, View::where('viewable_id', $post->getKey())->count());
+
+        $post->delete();
+
+        $this->assertEquals(0, View::where('viewable_id', $post->getKey())->count());
+    }
+}
