a284d2ececd137b61b7f0688d525a351f4a3d7af
Cyril de Wit <cyrilwit@gmail.com>
2018-03-26 17:33:53 +0200

    feat: add wrapper to retrieve the user's ip address

---
+++ src/Support/Ip.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Eloquent Viewable.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable\Support;
+
+use Request;
+
+/**
+ * Class Ip.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+class Ip
+{
+    /**
+     * Get the visitor's ip address.
+     *
+     * @return bool
+     */
+    public function get()
+    {
+        return Request::ip();
+    }
+}
