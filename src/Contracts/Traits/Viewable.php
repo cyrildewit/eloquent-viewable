6aac5543d3e55272c894e0737affbe207d41a038
Cyril de Wit <cyrilwit@gmail.com>
2018-04-03 13:02:39 +0200

    refactor: remove superfluous methods from Viewable trait

--- src/Contracts/Traits/Viewable.php
+++ src/Contracts/Traits/Viewable.php
@@ -45,171 +45,19 @@ interface Viewable
     public function views(): MorphMany;
 
     /**
-     * Get the total number of views.
-     *
-     * @return int
-     */
-    public function getViews(): int;
-
-    /**
-     * Get the total number of views since the given date.
-     *
-     * @param  \Carbon\Carbon  $sinceDateTime
-     * @return int
-     */
-    public function getViewsSince($sinceDateTime): int;
-
-    /**
-     * Get the total number of views upto the given date.
-     *
-     * @param  \Carbon\Carbon  $uptoDateTime
-     * @return int
-     */
-    public function getViewsUpto($uptoDateTime): int;
-
-    /**
-     * Get the total number of views between the given dates.
-     *
-     * @param  \Carbon\Carbon  $sinceDateTime
-     * @param  \Carbon\Carbon  $uptoDateTime
-     * @return int
-     */
-    public function getViewsBetween($sinceDateTime, $uptoDateTime): int;
-
-    /**
      * Get the total number of unique views.
      *
+     * @param  \CyrildeWit\EloquentViewable\Support\Period
      * @return int
      */
-    public function getUniqueViews(): int;
-
-    /**
-     * Get the total number of unique views since the given date.
-     *
-     * @param  \Carbon\Carbon  $sinceDateTime
-     * @return int
-     */
-    public function getUniqueViewsSince($sinceDateTime): int;
-
-    /**
-     * Get the total number of unique views upto the given date.
-     *
-     * @param  \Carbon\Carbon  $uptoDateTime
-     * @return int
-     */
-    public function getUniqueViewsUpto($uptoDateTime): int;
-
-    /**
-     * Get the total number of unique views upto the given date.
-     *
-     * @param  \Carbon\Carbon  $sinceDateTime
-     * @param  \Carbon\Carbon  $uptoDateTime
-     * @return int
-     */
-    public function getUniqueViewsBetween($sinceDateTime, $uptoDateTime): int;
-
-    /**
-     * Get the total number of views in the past 'n' seconds.
-     *
-     * @param  int  $seconds
-     * @return int
-     */
-    public function getViewsOfPastSeconds(int $seconds): int;
-
-    /**
-     * Get the total number of views in the past 'n' minutes.
-     *
-     * @param  int  $minutes
-     * @return int
-     */
-    public function getViewsOfPastMinutes(int $minutes): int;
-
-    /**
-     * Get the total number of views in the past 'n' days.
-     *
-     * @param  int  $days
-     * @return int
-     */
-    public function getViewsOfPastDays(int $days): int;
-
-    /**
-     * Get the total number of views in the past 'n' weeks.
-     *
-     * @param  int  $weeks
-     * @return int
-     */
-    public function getViewsOfPastWeeks(int $weeks): int;
-
-    /**
-     * Get the total number of views in the past 'n' months.
-     *
-     * @param  int  $months
-     * @return int
-     */
-    public function getViewsOfPastMonths(int $months): int;
-
-    /**
-     * Get the total number of views in the past 'n' years.
-     *
-     * @param  int  $years
-     * @return int
-     */
-    public function getViewsOfPastYears(int $years): int;
-
-    /**
-     * Get the total number of views in the past 'n' seconds.
-     *
-     * @param  int  $seconds
-     * @return int
-     */
-    public function getUniqueViewsOfPastSeconds(int $seconds): int;
-
-    /**
-     * Get the total number of views in the past 'n' minutes.
-     *
-     * @param  int  $minutes
-     * @return int
-     */
-    public function getUniqueViewsOfPastMinutes(int $minutes): int;
-
-    /**
-     * Get the total number of views in the past 'n' days.
-     *
-     * @param  int  $days
-     * @return int
-     */
-    public function getUniqueViewsOfPastDays(int $days): int;
-
-    /**
-     * Get the total number of views in the past 'n' weeks.
-     *
-     * @param  int  $weeks
-     * @return int
-     */
-    public function getUniqueViewsOfPastWeeks(int $weeks): int;
-
-    /**
-     * Get the total number of views in the past 'n' months.
-     *
-     * @param  int  $months
-     * @return int
-     */
-    public function getUniqueViewsOfPastMonths(int $months): int;
-
-    /**
-     * Get the total number of views in the past 'n' years.
-     *
-     * @param  int  $years
-     * @return int
-     */
-    public function getUniqueViewsOfPastYears(int $years): int;
+    public function getUniqueViews($period = null) : int;
 
     /**
      * Store a new view.
      *
      * @return bool
      */
-    public function addView(): bool;
+    public function addView() : bool;
 
     /**
      * Get the total number of views.
