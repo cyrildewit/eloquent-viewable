1bb6975bc8ecfbc156c14210c974d6ddc791c9ff
Cyril de Wit <cyrilwit@gmail.com>
2018-06-06 13:57:04 +0200

    feat: add ViewSessionHistory class

---
+++ src/ViewSessionHistory.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Eloquent Viewable package.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Session\Session;
+
+/**
+ * Class ViewSessionHistory.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+class ViewSessionHistory
+{
+    /**
+     * The session repository instance.
+     *
+     * @var \Illuminate\Contracts\Session\Repository
+     */
+    protected $session;
+
+    /**
+     * @var string
+     */
+    protected $primaryKey;
+
+    /**
+     * Create a new view session history instance.
+     */
+    public function __construct()
+    {
+        $this->session = app(Session::class);
+        $this->primaryKey = config('eloquent-viewable.session.key', 'cyrildewit.eloquent-viewable.session');
+    }
+
+    /**
+     * Push a viewable model with an expiry date to the session.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $viewable
+     * @param  \DateTime  $expiryDateTime
+     */
+    public function pushViewable($viewable, $expiryDateTime)
+    {
+        $baseKey = $this->primaryKey.'.'.strtolower(str_replace('\\', '-', $viewable->getMorphClass()));
+
+        $this->removeExpiredViews($baseKey);
+
+        // Check if the viewable model has been viewed, otherwise don't add it
+        // to the session
+        if (! $this->isViewableViewed($baseKey, $viewable->getKey())) {
+            $this->session->push($baseKey, [
+                'viewable_id' => $viewable->getKey(),
+                'expires_at' => $expiryDateTime,
+            ]);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the given model has been visited.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    protected function isViewableViewed(string $key, $viewableId)
+    {
+        $viewHistory = $this->session->get($key, []);
+
+        foreach ($viewHistory as $record) {
+            if ($record['viewable_id'] === $viewableId) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Remove all expired views from the session.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    protected function removeExpiredViews(string $key)
+    {
+        $currentTime = Carbon::now();
+        $history = $this->session->get($key, []);
+
+        foreach ($history as $item) {
+            // Less thatn or equal to
+            if ($item['expires_at']->lte($currentTime)) {
+                $itemId = array_search($item['viewable_id'], array_column($item, 'viewable_id'));
+
+                $this->session->pull($key.$itemId);
+            }
+        }
+    }
+}
