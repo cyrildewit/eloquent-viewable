9185b46ef43aa51ffc513aded66d6805d9e77b42
Cyril de Wit <cyrilwit@gmail.com>
2018-08-03 14:03:13 +0200

    style: apply fixes from StyleCI (#93)
    [ci skip]

--- tests/Unit/ViewsTest.php
+++ tests/Unit/ViewsTest.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace CyrildeWit\EloquentViewable\Tests\Unit;
 
-use Config;
 use CyrildeWit\EloquentViewable\View;
 use CyrildeWit\EloquentViewable\Views;
 use CyrildeWit\EloquentViewable\Tests\TestCase;
