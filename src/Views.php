82ec56cb56d9043e4917087f7d2fdfb7ca28892e
Cyril de Wit <cyrilwit@gmail.com>
2018-08-03 13:51:57 +0200

    feat: add static getViewsByType and getUniqueViewsByType methods

--- src/Views.php
+++ src/Views.php
@@ -13,7 +13,9 @@ declare(strict_types=1);
 
 namespace CyrildeWit\EloquentViewable;
 
+use CyrildeWit\EloquentViewable\View;
 use Illuminate\Database\Eloquent\Model;
+use CyrildeWit\EloquentViewable\Support\Period;
 
 /**
  * Class Views.
@@ -32,5 +34,79 @@ class Views
     public static function create($viewable, string $tag): self
     {
         return new static($viewable);
+    }
+
+    /**
+     * Get the total number of views of a viewable type.
+     *
+     * @param string|Illuminate\Database\Eloquent\Model  $viewableType
+     * @param  \CyrildeWit\EloquentViewable\Support\Period
+     * @return int
+     */
+    public static function getViewsByType($viewableType, $period = null): int
+    {
+
+        if ($viewableType instanceof Model) {
+            $viewableType = $viewableType->getMorphClass();
+        }
+
+        return $this->countViewsByType($viewableType, $period);
+    }
+
+    /**
+     * Get the total number of unique views of a viewable type.
+     *
+     * @param string|Illuminate\Database\Eloquent\Model  $viewableType
+     * @param  \CyrildeWit\EloquentViewable\Support\Period
+     * @return int
+     */
+    public static function getUniqueViewsByType($viewableType, $period = null): int
+    {
+        if ($viewableType instanceof Model) {
+            $viewableType = $viewableType->getMorphClass();
+        }
+
+        return $this->countViewsByType($viewableType, $period, true);
+    }
+
+    /**
+     * Count the views of a specific viewable type
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $viewable
+     * @param  \CyrildeWit\EloquentViewable\Support\Period
+     * @param  bool  $unique
+     * @return int
+     */
+    protected function countViewsByType($viewableType, $period, bool $unique = false)
+    {
+        // Use inserted period, otherwise create an empty one
+        $period = $period ?? Period::create();
+
+        $startDateTime = $period->getStartDateTime();
+        $endDateTime = $period->getEndDateTime();
+
+        // Create new Query Builder instance of the views table
+        $query = View::where('viewable_type', $viewableType);
+
+        // Apply the following date filters
+        if ($startDateTime && ! $endDateTime) {
+            $query->where('viewed_at', '>=', $startDateTime);
+        } elseif (! $startDateTime && $endDateTime) {
+            $query->where('viewed_at', '<=', $endDateTime);
+        } elseif ($startDateTime && $endDateTime) {
+            $query->whereBetween('viewed_at', [$startDateTime, $endDateTime]);
+        }
+
+        // Count all the views
+        if (! $unique) {
+            $viewsCount = $query->count();
+        }
+
+        // Count only the unique views
+        if ($unique) {
+            $viewsCount = $query->distinct('visitor')->count('visitor');
+        }
+
+        return $viewsCount;
     }
 }
