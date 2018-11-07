cf2869775b00914dce06df5cfb720bc837918edf
Cyril de Wit <cyrilwit@gmail.com>
2018-11-07 19:59:39 +0100

    feat: move Period class to root

--- src/Period.php
+++ src/Period.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace CyrildeWit\EloquentViewable\Support;
+namespace CyrildeWit\EloquentViewable;
 
 use DateTime;
 use Carbon\Carbon;
