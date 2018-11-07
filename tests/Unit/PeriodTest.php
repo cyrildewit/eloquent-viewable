9f86d03cd6682daeb309b7012ffe9c0f99defb45
Cyril de Wit <cyrilwit@gmail.com>
2018-11-07 20:51:12 +0100

    test: Period and ViewSessionHistory

---
+++ tests/Unit/PeriodTest.php
@@ -0,0 +1,247 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Eloquent Viewable package.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable\Tests\Unit\Support;
+
+use Carbon\Carbon;
+use CyrildeWit\EloquentViewable\View;
+use CyrildeWit\EloquentViewable\Period;
+use CyrildeWit\EloquentViewable\Tests\TestCase;
+use CyrildeWit\EloquentViewable\Exceptions\InvalidPeriod;
+
+/**
+ * Class PeriodTest.
+ *
+ * @author Cyril de Wit <github@cyrildewit.nl>
+ */
+class PeriodTest extends TestCase
+{
+    protected function tearDown()
+    {
+        Carbon::setTestNow();
+    }
+
+    /** @test */
+    public function it_can_instantiate_helper()
+    {
+        $helper = $this->app->make(Period::class);
+
+        $this->assertInstanceOf(Period::class, $helper);
+    }
+
+    /** @test */
+    public function it_can_construct_a_new_period_instance()
+    {
+        $startDateTime = Carbon::yesterday();
+        $endDateTime = Carbon::today();
+
+        $period = new Period($startDateTime, $endDateTime);
+
+        $this->assertEquals($period->getStartDateTime(), $startDateTime);
+        $this->assertEquals($period->getEndDateTime(), $endDateTime);
+    }
+
+    /** @test */
+    public function it_will_throw_an_exception_if_the_start_date_time_comes_after_the_end_date_time()
+    {
+        $startDateTime = Carbon::create(2018, 1, 1);
+        $endDateTime = Carbon::create(2017, 1, 1);
+
+        $this->expectException(InvalidPeriod::class);
+
+        $period = new Period($startDateTime, $endDateTime);
+    }
+
+    /** @test */
+    public function static_create_can_construct_a_new_period_instance()
+    {
+        $startDateTime = Carbon::yesterday();
+        $endDateTime = Carbon::today();
+
+        $period = Period::create($startDateTime, $endDateTime);
+
+        $this->assertEquals($period->getStartDateTime(), $startDateTime);
+        $this->assertEquals($period->getEndDateTime(), $endDateTime);
+    }
+
+    /** @test */
+    public function static_since_can_construct_a_new_period_instance()
+    {
+        $startDateTime = Carbon::yesterday();
+
+        $period = Period::since($startDateTime);
+
+        $this->assertEquals($period->getStartDateTime(), $startDateTime);
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_upto_can_construct_a_new_period_instance()
+    {
+        $endDateTime = Carbon::yesterday();
+
+        $period = Period::upto($endDateTime);
+
+        $this->assertNull($period->getStartDateTime());
+        $this->assertEquals($period->getEndDateTime(), $endDateTime);
+    }
+
+    /** @test */
+    public function static_pastDays_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::pastDays(5);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::today()->subDays(5));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_pastWeeks_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::pastWeeks(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::today()->subWeeks(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_pastMonths_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::pastMonths(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::today()->subMonths(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_pastYears_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::pastYears(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::today()->subYears(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_subSeconds_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::subSeconds(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::now()->subSeconds(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_subMinutes_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::subMinutes(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::now()->subMinutes(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_subHours_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::subHours(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::now()->subHours(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_subDays_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::subDays(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::now()->subDays(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_subWeeks_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::subWeeks(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::now()->subWeeks(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_subMonths_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::subMonths(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::now()->subMonths(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function static_subYears_can_construct_a_new_period_instance()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::subYears(2);
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::now()->subYears(2));
+        $this->assertNull($period->getEndDateTime());
+    }
+
+    /** @test */
+    public function setStartDateTime_can_set_a_new_start_date_time()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::create();
+
+        $this->assertNull($period->getStartDateTime());
+
+        $period->setStartDateTime(Carbon::now());
+
+        $this->assertEquals($period->getStartDateTime(), Carbon::now());
+    }
+
+    /** @test */
+    public function setEndDateTime_can_set_a_new_start_date_time()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $period = Period::create();
+
+        $this->assertNull($period->getEndDateTime());
+
+        $period->setEndDateTime(Carbon::now());
+
+        $this->assertEquals($period->getEndDateTime(), Carbon::now());
+    }
+}
