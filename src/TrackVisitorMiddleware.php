695eeaabfb721d086fb3934d291f1e3ee04240bc
Cyril de Wit <cyrilwit@gmail.com>
2018-11-10 15:15:09 +0100

    feat: add TrackVisitorMiddleare

---
+++ src/TrackVisitorMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Eloquent Viewable package.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable;
+
+use Closure;
+
+class TrackVisitorMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $cookieName = config('eloquent-viewable.visitor_cookie_key');
+        $uniqueId = str_random(80);
+
+        return $response->withCookie(cookie()->forever($cookieName, $uniqueId));
+    }
+}
