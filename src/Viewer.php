fb70fd199bc52a0833d7623b8367398b7c1ce489
Cyril de Wit <cyrilwit@gmail.com>
2019-07-05 15:45:09 +0200

    feat: add Viewer class with ip and isCrawler

---
+++ src/Viewer.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Eloquent Viewable package.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable;
+
+use Illuminate\Http\Request;
+use CyrildeWit\EloquentViewable\Contracts\CrawlerDetector;
+
+class Viewer
+{
+    /**
+     * The request instance.
+     *
+     * @var \Illuminate\Http\Request
+     */
+    protected $request;
+
+    /**
+     * The crawler detector instance.
+     *
+     * @var \CyrildeWit\EloquentViewable\Contracts\CrawlerDetector
+     */
+    protected $crawlerDetector;
+
+    /**
+     * Create a new viewer instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \CyrildeWit\EloquentViewable\Contracts\CrawlerDetector  $crawlerDetector
+     * @return void
+     */
+    public function __construct(Request $request, CrawlerDetector $crawlerDetector)
+    {
+        $this->request = $request;
+        $this->crawlerDetector = $crawlerDetector;
+    }
+
+    /**
+     * Get the viewer IP address.
+     *
+     * @return string|null
+     */
+    public function ip()
+    {
+        return $this->request->ip();
+    }
+
+    /**
+     * Determine if the viewer is a crawler.
+     *
+     * @return bool
+     */
+    public function isCrawler(): bool
+    {
+        return $this->crawlerDetector->isCrawler();
+    }
+}
---
+++ tests/Feature/ViewerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Eloquent Viewable package.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CyrildeWit\EloquentViewable\Tests\Feature;
+
+use Mockery;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use CyrildeWit\EloquentViewable\Viewer;
+use CyrildeWit\EloquentViewable\Support\Period;
+use CyrildeWit\EloquentViewable\Tests\TestCase;
+use CyrildeWit\EloquentViewable\Tests\TestHelper;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use CyrildeWit\EloquentViewable\Tests\Stubs\Models\Post;
+use CyrildeWit\EloquentViewable\Contracts\CrawlerDetector;
+
+class ViewerTest extends TestCase
+{
+    /** @test */
+    public function it_can_get_the_ip_address_from_the_request()
+    {
+        $this->mock(Request::class, function ($mock) {
+            $mock->shouldReceive('ip')->once()->andReturn('241.224.55.106');
+        });
+
+        $viewer = app(Viewer::class);
+
+        $this->assertEquals('241.224.55.106', $viewer->ip());
+    }
+
+    /** @test */
+    public function it_can_determine_if_the_viewer_is_a_crawler()
+    {
+        $this->mock(CrawlerDetector::class, function ($mock) {
+            $mock->shouldReceive('isCrawler')->once()->andReturn(true);
+        });
+
+        $viewer = app(Viewer::class);
+
+        $this->assertTrue($viewer->isCrawler());
+    }
+}
