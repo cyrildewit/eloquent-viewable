65364d41eac3e25189362cc40e9afc41cf06076d
Cyril de Wit <cyrilwit@gmail.com>
2019-07-05 19:18:00 +0200

    test: Viewer class

--- tests/Feature/ViewerTest.php
+++ tests/Feature/ViewerTest.php
@@ -16,10 +16,23 @@ namespace CyrildeWit\EloquentViewable\Tests\Feature;
 use Illuminate\Http\Request;
 use CyrildeWit\EloquentViewable\Viewer;
 use CyrildeWit\EloquentViewable\Tests\TestCase;
+use CyrildeWit\EloquentViewable\VisitorCookieRepository;
 use CyrildeWit\EloquentViewable\Contracts\CrawlerDetector;
 
 class ViewerTest extends TestCase
 {
+    /** @test */
+    public function it_can_get_the_id_from_the_visitor_cookie_repository()
+    {
+        $this->mock(VisitorCookieRepository::class, function ($mock) {
+            $mock->shouldReceive('get')->once()->andReturn('241.224.55.106');
+        });
+
+        $viewer = app(Viewer::class);
+
+        $this->assertEquals('241.224.55.106', $viewer->id());
+    }
+
     /** @test */
     public function it_can_get_the_ip_address_from_the_request()
     {
@@ -33,7 +46,19 @@ class ViewerTest extends TestCase
     }
 
     /** @test */
-    public function it_can_determine_if_the_viewer_is_a_crawler()
+    public function it_can_determine_if_the_viewer_has_a_do_not_tracker_header_from_the_request()
+    {
+        $this->mock(Request::class, function ($mock) {
+            $mock->shouldReceive('header')->once()->andReturn('1');
+        });
+
+        $viewer = app(Viewer::class);
+
+        $this->assertTrue($viewer->hasDoNotTrackHeader());
+    }
+
+    /** @test */
+    public function it_can_determine_if_the_viewer_is_a_crawler_from_the_crawler_detector()
     {
         $this->mock(CrawlerDetector::class, function ($mock) {
             $mock->shouldReceive('isCrawler')->once()->andReturn(true);
