513f021e20285ca5b248476421b50718b055ae38
Cyril de Wit <cyrilwit@gmail.com>
2019-08-23 15:25:59 +0200

    refactor: move Views facade to top level

--- src/ViewsFacade.php
+++ src/ViewsFacade.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace CyrildeWit\EloquentViewable\Facades;
+namespace CyrildeWit\EloquentViewable;
 
 use Illuminate\Support\Facades\Facade;
 use CyrildeWit\EloquentViewable\Contracts\Views as ViewsContract;
@@ -19,7 +19,7 @@ use CyrildeWit\EloquentViewable\Contracts\Views as ViewsContract;
 /**
  * @see \CyrildeWit\EloquentViewable\Views
  */
-class Views extends Facade
+class ViewsFacade extends Facade
 {
     /**
      * Get the registered name of the component.
