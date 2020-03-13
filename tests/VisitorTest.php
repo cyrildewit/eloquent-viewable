d81808a378a39db97b13fa75cb9cbb203cecd7ad
Cyril de Wit <cyrilwit@gmail.com>
2020-03-13 15:04:36 +0100

    test: remove unneccesary test

--- tests/VisitorTest.php
+++ tests/VisitorTest.php
@@ -13,18 +13,6 @@ use Illuminate\Http\Request;
 class VisitorTest extends TestCase
 {
     /** @test */
-    public function it_can_get_the_id_from_the_visitor_cookie_repository()
-    {
-        $this->mock(VisitorCookieRepository::class, function ($mock) {
-            $mock->shouldReceive('get')->once()->andReturn('241.224.55.106');
-        });
-
-        $visitor = Container::getInstance()->make(Visitor::class);
-
-        $this->assertEquals('241.224.55.106', $visitor->id());
-    }
-
-    /** @test */
     public function it_can_get_the_ip_address_from_the_request()
     {
         $this->mock(Request::class, function ($mock) {
