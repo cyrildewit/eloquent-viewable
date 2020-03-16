ff4aeb6c95327d2ce54b1ae4947821f5bfe2a9f6
Cyril de Wit <cyrilwit@gmail.com>
2020-03-16 11:46:35 +0100

    test: improve helper

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -6,20 +6,22 @@ namespace CyrildeWit\EloquentViewable\Tests;
 
 use Carbon\Carbon;
 use CyrildeWit\EloquentViewable\View;
+use CyrildeWit\EloquentViewable\Contracts\Viewable;
 
 class TestHelper
 {
     /**
      * Helper function to create a view.
      *
+     * @param  \CyrildeWit\EloquentViewable\Contracts\Viewable  $viewable
      * @param  array  $data
      * @return \CyrildeWit\EloquentViewable\View
      */
-    public static function createView($model, $data = [])
+    public static function createView(Viewable $viewable, $data = [])
     {
         return View::create([
-            'viewable_id' => $model->getKey(),
-            'viewable_type' => $model->getMorphClass(),
+            'viewable_id' => $viewable->getKey(),
+            'viewable_type' => $viewable->getMorphClass(),
             'visitor' => $data['visitor'] ?? 'unique_hash',
             'collection' => $data['collection'] ?? null,
             'viewed_at' => $data['viewed_at'] ?? Carbon::now(),
