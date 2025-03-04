8946f252ac064222a9018477a1b3a14f3792d65d
Cyril de Wit <cyrilwit@gmail.com>
2025-03-04 03:04:46 +0100

    test: modernize tests with new practices and learnings

--- tests/CooldownManagerTest.php
+++ tests/CooldownManagerTest.php
@@ -8,12 +8,11 @@ use Carbon\Carbon;
 use CyrildeWit\EloquentViewable\CooldownManager;
 use CyrildeWit\EloquentViewable\Tests\TestClasses\Models\Post;
 use Illuminate\Container\Container;
-use Session;
+use Illuminate\Support\Facades\Session;
 
 class CooldownManagerTest extends TestCase
 {
-    /** @test */
-    public function push_can_add_an_item()
+    public function push_can_add_an_item(): void
     {
         $post = Post::factory()->create();
         $cooldownManager = Container::getInstance()->make(CooldownManager::class);
@@ -28,8 +27,7 @@ class CooldownManagerTest extends TestCase
         $this->assertTrue(Session::has($postSessionKey));
     }
 
-    /** @test */
-    public function push_can_add_an_item_with_collection()
+    public function push_can_add_an_item_with_collection(): void
     {
         $post = Post::factory()->create();
         $cooldownManager = Container::getInstance()->make(CooldownManager::class);
@@ -42,8 +40,7 @@ class CooldownManagerTest extends TestCase
         $this->assertTrue(Session::has($postSessionKey));
     }
 
-    /** @test */
-    public function push_does_not_add_an_item_if_already_added()
+    public function push_does_not_add_an_item_if_already_added(): void
     {
         $post = Post::factory()->create();
         $postBaseKey = Container::getInstance()->make('config')->get('eloquent-viewable.cooldown.key').'.'.strtolower(str_replace('\\', '-', $post->getMorphClass()));
@@ -56,26 +53,24 @@ class CooldownManagerTest extends TestCase
         $this->assertCount(1, Session::get($postBaseKey));
     }
 
-    /** @test */
-    public function it_can_forget_expired_views()
+    public function test_it_can_forget_expired_views(): void
     {
         $post = Post::factory()->create();
-        $postNamespacKey = Container::getInstance()->make('config')->get('eloquent-viewable.cooldown.key').'.'.strtolower(str_replace('\\', '-', $post->getMorphClass()));
+        $postNamespaceKey = Container::getInstance()->make('config')->get('eloquent-viewable.cooldown.key').'.'.strtolower(str_replace('\\', '-', $post->getMorphClass()));
         $cooldownManager = Container::getInstance()->make(CooldownManager::class);
 
         $cooldownManager->push($post, Carbon::today());
-        $cooldownManager->push($post, Carbon::today()->addHours(1));
+        $cooldownManager->push($post, Carbon::today()->addHour());
         $cooldownManager->push($post, Carbon::today()->addHours(2));
 
         Carbon::setTestNow(Carbon::tomorrow());
 
         $cooldownManager->push($post, Carbon::today()->addHours(2));
 
-        $this->assertCount(1, Session::get($postNamespacKey));
+        $this->assertCount(1, Session::get($postNamespaceKey));
     }
 
-    /** @test */
-    public function it_can_forget_expired_views_with_collection()
+    public function test_it_can_forget_expired_views_with_collection(): void
     {
         $post = Post::factory()->create();
         $postNamespacKey = Container::getInstance()->make('config')->get('eloquent-viewable.cooldown.key').'.'.strtolower(str_replace('\\', '-', $post->getMorphClass()));
@@ -83,7 +78,7 @@ class CooldownManagerTest extends TestCase
 
         $cooldownManager->push($post, Carbon::today());
         $cooldownManager->push($post, Carbon::today(), 'some-collection');
-        $cooldownManager->push($post, Carbon::today()->addHours(1));
+        $cooldownManager->push($post, Carbon::today()->addHour());
         $cooldownManager->push($post, Carbon::today()->addHours(2));
         $cooldownManager->push($post, Carbon::today()->addHours(2), 'some-collection');
 
